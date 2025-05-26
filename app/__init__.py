d0a2b73e708f51500ef7339a71e7f448ed3bd830
fr3d98 <37631735+fr3d98@users.noreply.github.com>
2025-05-26 17:08:22 +0200

    removed EXAMPLE credentials on __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.admin import bp as admin_bp
 from app.extensions import db, login_manager, migrate
 from flask_ckeditor import CKEditor
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -13,8 +14,8 @@ def create_app():
     app = Flask(__name__)
     #Add CKEditor
     ckeditor = CKEditor(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password123@localhost/users'
-    app.config['SECRET_KEY'] = "seckey"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     
     # Inizializza estensioni
     db.init_app(app)
